ef1189f2705d603e9e17fd0303137ca28b9df206
Mehmet Türker ÖZAKINCI <airtur@hotmail.com>
2021-01-31 00:20:44 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Abstract; //implement ettik - işaret ettik
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //çıplak class kalmasın
+    public class Category: IEntity //implement ettik - işaret ettik
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
