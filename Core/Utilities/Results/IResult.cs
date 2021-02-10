e97113ece0b90a8bae3e3ca455150beafb5ff5fc
Mehmet Türker ÖZAKINCI <airtur@hotmail.com>
2021-02-11 00:14:27 +0300

    10 şubat ders sonu

---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    //temel voidler için başlangıç
+    public interface IResult
+    {
+        bool Success { get; } //yapmaya çalıştığın iş başarılı mı
+        string Message { get; } //o iş için mesaj ver kullanıcıya
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        
+        public Result(bool success, string message): this(success) //success i buraya atar.
+        {
+            Message = message;
+        }
+
+        public Result(bool success) //overloading - birden fazla aynı isimli method
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
